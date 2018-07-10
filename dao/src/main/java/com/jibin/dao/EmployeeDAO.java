11c257f12c0a1b20fbba5c33784b593ef10b6059
吉彬 <jibin_0310@163.com>
2018-07-10 23:41:47 +0800

    编写了DAO接口

--- dao/src/main/java/com/jibin/dao/EmployeeDAO.java
+++ dao/src/main/java/com/jibin/dao/EmployeeDAO.java
@@ -1,4 +1,32 @@
 package com.jibin.dao;
+import com.jibin.entity.Employee;
+import java.util.List;
 
 public interface EmployeeDAO {
+    /**
+     * 查询所有雇员信息。
+     * @return 查询结果封装的List集合。
+     */
+    public List<Employee> findEmployee();
+
+    /**
+     * 根据id查询雇员。
+     * @param id 雇员编号。
+     * @return 该编号对应的雇员对象。
+     */
+    public Employee findEmployeeById(Integer id);
+
+    /**
+     * 根据id删除雇员信息。
+     * @param id 要删除的雇员id。
+     * @return 被删除的雇员id。
+     */
+    public Integer deleteEmployeeById(Integer id);
+
+    /**
+     * 插入雇员信息。
+     * @param employee 封装了要插入雇员信息的雇员对象。
+     * @return true：插入成功；false：插入失败。
+     */
+    public boolean insertEmployee(Employee employee);
 }
